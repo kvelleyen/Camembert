6d23b5d226667c64bcaeac9aa8ee3a167f1f4b83
kvelleyen <kevin.velleyen@hotmail.fr>
2014-11-18 15:37:25 +0100

    final

--- src/Controller.java
+++ src/Controller.java
@@ -10,16 +10,18 @@ public class Controller extends JFrame{
 	int X;
 	int Y;
 
+	int indiceAffichage;
+
 	static View vue;
 	static Model myModel;
 
 	public Controller(){
 		myModel = new Model();
 		myModel.setTitre("Mon Budget");
-		myModel.setChamps(new String[]{"Loyer","Alimentation","Cinema","Mutuelle"});
+		myModel.setChamps(new String[]{"Loyer","Alimentation","Cinema","Mutuelle","Jardinage"});
 		myModel.setDescription(new String[]{"Loyer minimal",
-				"Quotidien, RU","Avengers, Spiderman","Sécurité social"});
-		myModel.setDonnees(new Double[]{150.0, 150.0, 75.0, 80.0});	
+				"Quotidien, RU","Avengers, Spiderman","Sécurité social","Pelle, rateau"});
+		myModel.setDonnees(new Double[]{150.0, 150.0, 75.0, 80.0, 150.0});	
 
 	}
 
@@ -40,33 +42,221 @@ public class Controller extends JFrame{
 	}
 
 	public void onClickDescription(){
-		
-		for(Polygon p : vue.getFleches()){
-			if(){}
+
+		if(! vue.getFleches().isEmpty()){
+			//Flèche précédente
+			if(vue.getFleches().get(0).contains(X,Y)){
+				for(Integer i : vue.getPortion().keySet()){
+					indiceAffichage = (i+1)%vue.getTableauChamps().length;
+					//System.out.println(indiceAffichage);
+					//System.err.println(vue.getTableauChamps()[indiceAffichage].getY());
+					if(indiceAffichage<0){indiceAffichage=indiceAffichage+vue.getTableauChamps().length;}
+					//premier quartier
+					if(vue.getTableauChamps()[indiceAffichage].getX() >= vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() < vue.getCentreY()){
+						if(vue.getDescription().size()==1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()+100,
+									(int)vue.getTableauChamps()[indiceAffichage].getY(),
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[i].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}//deuxieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() < vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() <= vue.getCentreY()){
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()-175,
+									(int)vue.getTableauChamps()[indiceAffichage].getY(),
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[i].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+						}
+					}//troisieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() < vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() >= vue.getCentreY()){
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()-175,
+									(int)vue.getTableauChamps()[indiceAffichage].getY()-40,
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[indiceAffichage].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}//quatrieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() > vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() >= vue.getCentreY()){
+
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()+100,
+									(int)vue.getTableauChamps()[indiceAffichage].getY()-40,
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[indiceAffichage].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}
+				}					
+			}//Flèche suivante			
+			else if(vue.getFleches().get(1).contains(X,Y)){
+				for(Integer i : vue.getPortion().keySet()){
+					indiceAffichage = (i-1)%vue.getTableauChamps().length;
+					//System.out.println(indiceAffichage);
+					//System.err.println(vue.getTableauChamps()[indiceAffichage].getY());
+					if(indiceAffichage<0){indiceAffichage=indiceAffichage+vue.getTableauChamps().length;}
+					//premier quartier
+					if(vue.getTableauChamps()[indiceAffichage].getX() >= vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() < vue.getCentreY()){
+						if(vue.getDescription().size()==1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()+100,
+									(int)vue.getTableauChamps()[indiceAffichage].getY(),
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[i].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}//deuxieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() < vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() <= vue.getCentreY()){
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()-175,
+									(int)vue.getTableauChamps()[indiceAffichage].getY(),
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[i].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+						}
+					}//troisieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() < vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() >= vue.getCentreY()){
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()-175,
+									(int)vue.getTableauChamps()[indiceAffichage].getY()-40,
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[indiceAffichage].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}//quatrieme quartier
+					else if (vue.getTableauChamps()[indiceAffichage].getX() > vue.getCentreX() && vue.getTableauChamps()[indiceAffichage].getY() >= vue.getCentreY()){
+
+						if(vue.getDescription().size() == 1){
+							Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[indiceAffichage].getX()+100,
+									(int)vue.getTableauChamps()[indiceAffichage].getY()-40,
+									vue.getTableauChamps()[indiceAffichage].width+75,
+									vue.getTableauChamps()[indiceAffichage].height+40);
+							vue.getDescription().put(indiceAffichage, rectangle);
+							vue.getDescription().remove(i);
+
+							Arc2D arc = new Arc2D.Double(vue.getTableauArc2D()[indiceAffichage].getX()-10,
+									vue.getTableauArc2D()[indiceAffichage].getY()-10,
+									vue.getTableauArc2D()[indiceAffichage].getWidth()+20,
+									vue.getTableauArc2D()[indiceAffichage].getHeight()+20,
+									vue.getTableauArc2D()[indiceAffichage].getAngleStart(),
+									vue.getTableauArc2D()[indiceAffichage].getAngleExtent()+2, Arc2D.PIE);
+
+							vue.getPortion().put(indiceAffichage, arc);
+							vue.getPortion().remove(i);
+
+						}
+					}
+				}					
+			
+				
+			}
 		}
-		
+
 		for (int i = 0; i < vue.getTableauChamps().length; i++) {
 			//Si on clique sur un rectangle ou sur un arc2D
 			if(vue.getTableauChamps()[i].contains(X,Y) || vue.getTableauArc2D()[i].contains(X,Y)){
-				
-			Polygon triangle1 = new Polygon();
-			Polygon triangle2 = new Polygon();
-			
-			triangle1.addPoint(950, 10);
-			triangle1.addPoint(930, 40);			
-			triangle1.addPoint(970, 40);
-			
 
-			triangle2.addPoint(950, 80);
-			triangle2.addPoint(930, 50);			
-			triangle2.addPoint(970, 50);
-			
-			vue.getFleches().add(triangle1);
-			vue.getFleches().add(triangle2);
-				
+				Polygon triangle1 = new Polygon();
+				Polygon triangle2 = new Polygon();
+
+				triangle1.addPoint(950, 10);
+				triangle1.addPoint(930, 40);			
+				triangle1.addPoint(970, 40);
+
+
+				triangle2.addPoint(950, 80);
+				triangle2.addPoint(930, 50);			
+				triangle2.addPoint(970, 50);
+
+				vue.getFleches().add(triangle1);
+				vue.getFleches().add(triangle2);
+
 				//premier quartier
 				if(vue.getTableauChamps()[i].getX() >= vue.getCentreX() && vue.getTableauChamps()[i].getY() < vue.getCentreY()){
-				
+
 					if(vue.getDescription().isEmpty()){
 						Rectangle rectangle = new Rectangle((int)vue.getTableauChamps()[i].getX()+100,
 								(int)vue.getTableauChamps()[i].getY(),
@@ -85,7 +275,7 @@ public class Controller extends JFrame{
 						vue.getDescription().clear();
 						vue.getPortion().clear();
 						vue.getFleches().clear();
-						
+
 					}
 
 				}
@@ -133,7 +323,7 @@ public class Controller extends JFrame{
 						vue.getDescription().clear();
 						vue.getPortion().clear();
 						vue.getFleches().clear();
-						}
+					}
 				}
 				//quatrieme quartier
 				else if (vue.getTableauChamps()[i].getX() > vue.getCentreX() && vue.getTableauChamps()[i].getY() >= vue.getCentreY()){
